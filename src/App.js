1db25042f918cb41e89438f341d97cb3efe587fb
Abolfazl <iabolfazl83@yahoo.com>
2025-03-02 23:32:13 +0330

    added putting cart data to firebase database.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
   const cartVisible = useSelector(state => state.uiSlice.isVisible);
-  // fetch("https://redux-shopping-page-d6ecb-default-rtdb.asia-southeast1.firebasedatabase.app/") E8
+  const cart = useSelector(state => state.cartSlice);
+
+  useEffect(() => {
+    fetch("https://redux-shopping-page-d6ecb-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json", {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+  }, [cart])
   return (
     <Layout>
       {
